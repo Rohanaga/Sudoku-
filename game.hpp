e5cfab4cd3bc71276d6727c1080838c231c976d7
Rohanaga <115742983+Rohanaga@users.noreply.github.com>
2023-10-28 04:37:03 +0530

    Create game.hpp

---
+++ game.hpp
@@ -0,0 +1,12 @@
+
+#pragma once
+#include <iostream>
+#include <regex>
+#include <sstream>
+#include "solver.hpp"
+#include "altproj.hpp"
+
+
+void playGame(int size, int nobs);
+
+void playAgainPrompt(int size, int nobs);
