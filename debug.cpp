ef71912d0908914e74b25db2c344c1f2c5dfd522
Rohanaga <115742983+Rohanaga@users.noreply.github.com>
2023-10-28 04:34:28 +0530

    Create debug.cpp

---
+++ debug.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include<ctime>
+#include "solver.hpp"
+#include "altproj.hpp"
+using namespace std;
+using namespace arma;
+
+int main(int argc, char **argv){
+  
+  srand(time(NULL));
+  int n = 16;
+  Board board = generatePuzzle(n,20);
+  cout << "now!\n";
+
+
+  Board matt = DR(board);
+
+  bool solve = matt.checkPuzzle();
+  if(!solve)
+    cout << "DR Failed to Solve" << endl;
+  else
+    matt.printPuzzle();
+  
+
+  
+}
