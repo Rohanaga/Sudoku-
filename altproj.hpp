e646616d18d733099cfa9b484ebb82d5400b35e9
Rohanaga <115742983+Rohanaga@users.noreply.github.com>
2023-10-28 04:33:47 +0530

    Create altproj.hpp

---
+++ altproj.hpp
@@ -0,0 +1,16 @@
+
+#pragma once
+#include "solver.hpp"
+#include <armadillo>
+
+#define TOL 1e-7
+#define MAXITS 1e4
+
+arma::cube RC1(const arma::cube& Q);
+arma::cube RC2(const arma::cube& Q);
+arma::cube RC3(const arma::cube& Q);
+arma::cube RC4(const arma::cube& Q);
+arma::cube RC5(const arma::cube&Q,  Board& board);
+Board cube2Board(const arma::cube& Q);
+
+Board DR(Board& board);
